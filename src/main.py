1c1b904d2f4c570f2bd92845faee1e3c7f0828b9
H-momo <hirao.momona.f4@s.mail.nagoya-u.ac.jp>
2024-06-15 23:27:06 +0900

    エラーとシリアル出力の修正
    * channel_id や user_id が未知のときにプログラムが止まってしまうのを修正
    * シリアル出力を増やした
    * シリアル出力の文章をわかりやすくした

--- src/main.py
+++ src/main.py
@@ -19,23 +19,22 @@ USER_DATA = dict()
 # if mention
 @app.event("app_mention")
 def handle_app_mention_events(body, say):
-    event = body["event"]
-
-    if (event["subtype"] == "bot_message" or event["subtype"] == "message_changed" or event["subtype"] == "message_deleted"):
+    event = body.get("event", {})
+    if (event == {} or event.get("edited") != None):
         return
 
     # get posted user name and icon
-    user_name = USER_DATA[event["user"]]["name"]
-    user_icon = USER_DATA[event["user"]]["img"]
+    if (USER_DATA.get(event.get("user")) == None):
+        init()
+    user_id = event.get("user")
+    user_name = USER_DATA.get(user_id).get("name", "unkown user")
+    user_icon = USER_DATA.get(event.get("user")).get("img")
 
     # get posted channel name
-    channel_id = event["channel"]
-    if (CHANNEL_DATA[channel_id] == None):
-        init();
-    try:
-        channel_name = CHANNEL_DATA[event["channel"]]
-    except KeyError:
-        channel_name = "unkown channel"
+    channel_id = event.get("channel")
+    if (CHANNEL_DATA.get(channel_id) == None):
+        init()
+    channel_name = CHANNEL_DATA.get(channel_id, "unkown channel")
 
     # get message url
     team_id = body.get("team_id")
@@ -43,12 +42,21 @@ def handle_app_mention_events(body, say):
     message_url = f"https://{team_id}.slack.com/archives/{channel_id}/p{timestamp}"
 
     # post message
-    say (
-        channel = POST_CHANNEL_ID,
-        username = f"🤖 {user_name}",
-        icon_url = user_icon,
-        text=f"<{message_url}|#{channel_name}>"
-    )
+    if (user_icon == None):
+        say (
+            channel = POST_CHANNEL_ID,
+            username = f"🤖 {user_name}",
+            text=f"<{message_url}|#{channel_name}>"
+        )
+    else:
+        say (
+            channel = POST_CHANNEL_ID,
+            username = f"🤖 {user_name}",
+            icon_url = user_icon,
+            text=f"<{message_url}|#{channel_name}>"
+        )
+
+    print(f"Posted: {message_url}")
 
 
 @app.event('message')
@@ -57,38 +65,46 @@ def handle_message_events(body, logger):
 
 
 def init():
-    print("initializing...")
+    print("Getting channel and user data...")
 
     # get channel data
     url = "https://slack.com/api/conversations.list?limit=999"
     headres = {"Authorization": "Bearer " + SLACK_BOT_TOKEN}
     response = requests.get(url, headers=headres)
     response_json = response.json()
+    if (response_json.get("channels") == None):
+        print("Couldn't get channel data.")
+        exit(1)
+
     global CHANNEL_DATA
-    for i in response_json["channels"]:
-        CHANNEL_DATA[i["id"]] = i["name"]
+    for channel in response_json["channels"]:
+        CHANNEL_DATA[channel["id"]] = channel["name"]
 
     # get user data
     url = "https://slack.com/api/users.list"
     headres = {"Authorization": "Bearer " + SLACK_BOT_TOKEN}
     response = requests.get(url, headers=headres)
     response_json = response.json()
+    if (response_json.get("members") == None):
+        print("Couldn't get user data.")
+        exit(1)
+
     global USER_DATA
-    for i in response_json["members"]:
+    for member in response_json["members"]:
         try:
-            USER_DATA[i["id"]] = {"name": i["real_name"], "img": i["profile"]["image_72"]}
+            USER_DATA[member["id"]] = {"name": member["real_name"], "img": member["profile"]["image_72"]}
         except KeyError:
-            USER_DATA[i["id"]] = {"name": i["name"], "img": i["profile"]["image_72"]}
+            USER_DATA[member["id"]] = {"name": member["name"], "img": member["profile"]["image_72"]}
             print("Error: KeyError of getting user name")
 
-    print("initialized!")
+    print("Got channel and user data")
 
 
 def main():
     print("⚡️ Started!")
     init()
 
-    print(f"Send message to {POST_CHANNEL_ID}")
+    print(f"The bot will send message to {POST_CHANNEL_ID}")
 
     SocketModeHandler(app, SLACK_APP_TOCKEN).start()
 
